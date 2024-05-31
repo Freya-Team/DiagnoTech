81975460ce77c89de8466718acf80f4b462c495e
mhmdfjr <moh.fajar1304@gmail.com>
2024-05-31 23:50:25 +0700

    ui: add card layanan component

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -8,20 +8,18 @@ const Button = ({text, theme}) => {
     if (theme == 'primary') {
         bgClass = 'bg-green-35'
         borderClass = 'none'
-        textClass = 'text-black text-base font-poppins'
+        textClass = 'text-white text-base font-poppins'
     } 
     else if (theme == 'secondary') {
         bgClass = 'bg-green-50'
         borderClass = 'none'
-        textClass = 'text-black text-base font-poppins'
+        textClass = 'text-white text-base font-poppins'
     }
     else if (theme == 'tertiary') {
         bgClass = 'bg-transparent'
-        borderClass = 'border-solid border-2 border-green-50'
+        borderClass = 'border-solid border-[1px] border-green-50'
         textClass = 'text-green-50 text-base font-poppins'
     }
-
-    console.log({theme}, {text})
 
     return (
         <button className={`py-3 px-4 m-2 rounded-full shadow-lg ${bgClass} ${borderClass} ${textClass}`}>
