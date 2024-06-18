47be4378ffc77554bf58d8e3d3dfe6341026089c
mhmdfjr <moh.fajar1304@gmail.com>
2024-06-18 11:04:51 +0700

    ui: add page about

---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,12 @@
+import NavBar from '@/Components/NavBar'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="bg-gray-50">
+        <NavBar />
+    </div>
+  )
+}
+
+export default About
